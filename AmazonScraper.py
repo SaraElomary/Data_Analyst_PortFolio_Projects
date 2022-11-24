029415c222f271f57982b1b16e084902b92ad3fb
Sara EL OMARY <49750362+SaraElomary@users.noreply.github.com>
2022-11-24 23:42:59 +0100

    Add files via upload

---
+++ AmazonScraper.py
@@ -0,0 +1,116 @@
+# import libraries 
+
+from bs4 import BeautifulSoup
+import requests
+import time
+import datetime
+
+import smtplib
+# Connect to Website and pull in data
+
+URL = 'https://www.amazon.com/Funny-Data-Systems-Business-Analyst/dp/B07FNW9FGJ/ref=sr_1_3?dchild=1&keywords=data%2Banalyst%2Btshirt&qid=1626655184&sr=8-3&customId=B0752XJYNL&th=1'
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+
+page = requests.get(URL, headers=headers)
+
+soup1 = BeautifulSoup(page.content, "html.parser")
+
+soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
+
+title = soup2.find(id='productTitle').get_text()
+
+price = soup2.find(id='priceblock_ourprice').get_text()
+
+
+print(title)
+print(price)
+                  
+                   
+# Clean up the data a little bit
+
+price = price.strip()[1:]
+title = title.strip()
+
+print(title)
+print(price)
+
+# Create a Timestamp 
+
+import datetime
+
+today = datetime.date.today()
+
+print(today)
+2021-08-21
+# write headers and data into the CSV file
+
+import csv 
+
+header = ['Title', 'Price', 'Date']
+data = [title, price, today]
+
+
+with open('AmazonWebScraperDataset.csv', 'w', newline='', encoding='UTF8') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    writer.writerow(data)
+    
+import pandas as pd
+
+df = pd.read_csv(r'C:\Users\alexf\AmazonWebScraperDataset.csv')
+
+print(df)
+#Appending data to the csv
+
+with open('AmazonWebScraperDataset.csv', 'a+', newline='', encoding='UTF8') as f:
+    writer = csv.writer(f)
+    writer.writerow(data)
+
+#Above code into one function
+
+
+def check_price():
+    URL = 'https://www.amazon.com/Funny-Data-Systems-Business-Analyst/dp/B07FNW9FGJ/ref=sr_1_3?dchild=1&keywords=data%2Banalyst%2Btshirt&qid=1626655184&sr=8-3&customId=B0752XJYNL&th=1'
+
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+
+    page = requests.get(URL, headers=headers)
+
+    soup1 = BeautifulSoup(page.content, "html.parser")
+
+    soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
+
+    title = soup2.find(id='productTitle').get_text()
+
+    price = soup2.find(id='priceblock_ourprice').get_text()
+
+    price = price.strip()[1:]
+    title = title.strip()
+
+    import datetime
+
+    today = datetime.date.today()
+    
+    import csv 
+
+    header = ['Title', 'Price', 'Date']
+    data = [title, price, today]
+
+    with open('AmazonWebScraperDataset.csv', 'a+', newline='', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        writer.writerow(data)
+ 
+    
+# Runs check_price after a set time and inputs data into your CSV
+
+while(True):
+    check_price()
+    time.sleep(86400)
+import pandas as pd
+
+df = pd.read_csv(r'C:\Users\Lenove\AmazonWebScraperDataset.csv')
+
+print(df)
+
+
